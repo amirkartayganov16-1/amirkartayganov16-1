9e4f6a94c5aa0696be789f3832e81605709b0cf5
amirkartayganov16-1 <100897206+amirkartayganov16-1@users.noreply.github.com>
2022-03-15 21:34:18 +0600

    Add files via upload

---
+++ turtle/__init__.py
@@ -0,0 +1,31 @@
+import turtle
+
+Pencil = turtle.Turtle()
+Background = turtle.Screen()
+Pencil.pencolor('red')
+Background.bgcolor('black')
+
+Pencil.pensize(1)
+a = 0
+b = 0
+
+Pencil.speed(0)
+Pencil.penup()
+Pencil.goto(0, 200)
+Pencil.pendown()
+
+while True:
+    Pencil.forward(a)
+    Pencil.right(b)
+    a += 3
+    b += 1
+    if b == 210:
+        break
+    Pencil.hideturtle()
+
+turtle.done()
+
+
+
+
+
